71c3dfee868ee7778762808934c0a8a7f9e0f6f0
ahmadfahmip <ahmad.fahmi.pratama@gmail.com>
2019-04-19 14:53:09 +0700

    Add base position finder method

--- src/main/java/com/sisterhore/crdt/CRDT.java
+++ src/main/java/com/sisterhore/crdt/CRDT.java
@@ -1,6 +1,7 @@
 package com.sisterhore.crdt;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class CRDT {
   private int siteId;
@@ -33,23 +34,52 @@ public class CRDT {
   }
 
   public void generateChar(char value, int index) {
-    ArrayList<Integer> posBefore;
-    ArrayList<Integer> posAfter;
+    ArrayList<Integer> posBefore = this.findPosBefore(index);
+    ArrayList<Integer> posAfter = this.findPosAfter(index);
     ArrayList<Integer> newPos;
 
     posBefore = this.struct.get(index-1).getPosition();
     posAfter = this.struct.get(index).getPosition();
   }
 
-  public ArrayList<Integer> generatePos(ArrayList<Integer> posBefore, ArrayList<Integer> posAfter) {
-    /**
-     * Misalkan [1] dan [1,5] = [1,2,5]
-     */
-    if (posBefore.get(0) - posAfter.get(1) > 1) {
+  /**
+   * @param index index where new char will be inserted
+   * @return the position before
+   *
+   * For example, string ['l','o','r','e','m',' ','i','p','s','u','m']
+   * We want to insert character 'x' between 'o' and 'r',
+   * which means between position 1 and 2 (starting from 0)
+   * and we will insert 'x' on position 2.
+   * So, we need to find position before position 2, that is 1.
+   */
+  private ArrayList<Integer> findPosBefore(int index) {
+    Char charBefore = this.struct.get(index-1);
+    return charBefore.getPosition();
+  }
 
+  private ArrayList<Integer> findPosAfter(int index) {
+    Char charAfter = this.struct.get(index);
+    if (charAfter == null) {
+      return new ArrayList<>();
     }
-    ArrayList<Integer> result = new ArrayList<>();
-    return result;
+    return charAfter.getPosition();
+  }
+
+  private ArrayList<Integer> generatePosBetween(ArrayList<Integer> posBefore, ArrayList<Integer> posAfter, ArrayList<Integer> posResult) {
+    int level1 = posBefore.get(0);
+    int level2 = posAfter.get(0);
+
+    if (level1 - level2 > 1) {
+      int newPos = level1 + 1; // Will be replaced later
+      posResult.add(newPos);
+      return posResult;
+    } else if (level1 - level2 == 1) {
+      posResult.add(level1);
+      ArrayList<Integer> newPosBefore = (ArrayList) posBefore.clone();
+      newPosBefore.remove(0);
+      return this.generatePosBetween(newPosBefore, posAfter, posResult);
+    }
+    return posResult;
   }
 
 }
