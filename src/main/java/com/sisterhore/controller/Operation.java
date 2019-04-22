63d09c38a398bd665f26c6104ccbbb63bfd8ebde
Adylan Roaffa <adylanrff@gmail.com>
2019-04-22 19:48:12 +0700

    fixed Operation class

--- src/main/java/com/sisterhore/controller/Operation.java
+++ src/main/java/com/sisterhore/controller/Operation.java
@@ -1,10 +1,12 @@
+package com.sisterhore.controller;
+
 import com.sisterhore.crdt.Char;
+import com.sisterhore.version.Version;
 
 enum OperationType {INSERT, DELETE}
 
 public class Operation {
-    private OperationType operationType;
-    private Char characterUsed;
-    private Integer version;
-    private Integer index;
+    public OperationType operationType;
+    public Char characterUsed;
+    public Version version;
 }
